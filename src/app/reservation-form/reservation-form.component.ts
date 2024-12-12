7859b3817ef5da22cae209b13729f03293368b00
swopnilll <swopnil@lims1.com>
2024-12-12 15:59:13 +1100

    Basic CRUD in service

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
-  styleUrls: ['./reservation-form.component.css']
+  styleUrls: ['./reservation-form.component.css'],
 })
 export class ReservationFormComponent implements OnInit {
-  
   reservationForm: FormGroup = new FormGroup({});
 
-  constructor(private formBuilder: FormBuilder){
-
-  }
+  constructor(
+    private formBuilder: FormBuilder,
+    private reservationService: ReservationService
+  ) {}
 
   ngOnInit(): void {
     this.reservationForm = this.formBuilder.group({
@@ -22,14 +25,14 @@ export class ReservationFormComponent implements OnInit {
       guestName: ['', Validators.required],
       guestEmail: ['', [Validators.required, Validators.email]],
       roomNumber: ['', Validators.required],
-    })
+    });
   }
 
+  onSubmit() {
+    if (this.reservationForm.valid) {
+      let reservation: Reservation = this.reservationForm.value;
 
-  onSubmit(){
-    if(this.reservationForm.valid){
-      console.log("Valid")
+      this.reservationService.addReservation(reservation);
     }
   }
-
 }
--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  private reservations: Reservation[] = [];
+
+  getReservations(): Reservation[] {
+    return this.reservations;
+  }
+
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find(reservation => reservation.id === id);
+  }
+
+  addReservation(reservation: Reservation ): void {
+    this.reservations.push(reservation);
+    console.log(this.reservations);
+  }
+
+  updateReservation(reservation: Reservation): void {
+    let index = this.reservations.findIndex(reservation => reservation.id === reservation.id);
+
+    this.reservations[index] = reservation;
+  }
+
+  deleteReservation(id: string): void {
+    let index = this.reservations.findIndex(reservation => reservation.id === id);
+
+    this.reservations.splice(index, 1);
+  }
+
 }
